d7920c8ba346a9cc72ea82e460684eca6a850d93
MaksymBohun2004 <maksym.bohun@ucu.edu.ua>
2021-11-02 14:05:00 +0200

    program fix

--- ЛАБ6/gamewho.py
+++ ЛАБ6/gamewho.py
@@ -1,154 +1,210 @@
-''' A game of guessing words '''
-from typing import List
-import random
+import calendar as cal
+import doctest
+def weekday_name(number: int) -> str:
+    """
+    Return a string representing a weekday
+    (one of "mon", "tue", "wed", "thu", "fri", "sat", "sun")
+    number : an integer in range [0, 6]
 
-def generate_grid() -> List[List[str]]:
+    >>> weekday_name(3)
+    'thu'
     """
-    Generates list of lists of letters - i.e. grid for the game.
-    e.g. [['I', 'G', 'E'], ['P', 'I', 'S'], ['W', 'M', 'G']]
-    """
-    letters = []
-    sub_grid = []
-    for i in range(3):
-        for j in range(3):
-            sub_grid.append(chr(random.randrange(65, 90 + j - j + i - i)))
-        letters.append(sub_grid)
-        sub_grid = []
-    return letters
-
-def get_words(file: str, text: List[str]) -> List[str]:
-    """
-    Reads the file f. Checks the words with rules and returns a list of words.
-    >>> get_words('en.txt', [el for el in 'wumrovkif'])
-    ['fork', 'form', 'forum', 'four', 'fowk', 'from', 'frow', 'irok', 'komi', \
-'kori', 'miro', 'miro', 'moki', 'ovum', 'work', 'worm', 'wouf']
-    """
-    print(text)
-    alph = []
-    for i in range(26):
-        alph.append(chr(97 + i))
-    for i in range(9):
-        if text[i] in alph:
-            try:
-                alph.remove(text[i])
-            except ValueError:
-                continue
-    try:
-        with open(file) as file:
-            counter = 0
-            words = []
-            for line in file:
-                breaking = False
-                line = line.lower()
-                line = line[:-1]
-                if len(line) < 4 or counter < 3:
-                    counter += 1
-                    continue
-                if text[4] in line:
-                    for i in range(len(alph)):
-                        if alph[i] in line:
-                            breaking = True
-                            break
-                        for j in range(9):
-                            if text.count(text[j]) < line.count(text[j]):
-                                breaking = True
-                                break
-                else:
-                    breaking = True
-                if breaking:
-                    continue
-                words.append(line)
-                counter += 1
-    except FileNotFoundError:
+    if number < 0 or number > 6:
         return None
-    return words
+    days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+    day = days[number]
+    return day
 
-def get_user_words() -> List[str]:
+def weekday(date: str) -> int:
     """
-    Gets words from user input and returns a list with these words.
-    Usage: enter a word or press ctrl+d to finish.
+    Return an integer representing a weekday
+    (0 represents monday and so on)
+    Read about algorithm as Zeller's Congruence
+    date : a string of form "day.month.year
+    if the date is invalid raises AssertionError
+    with corresponding message
+
+    >>> weekday("12.08.2015")
+    2
+    >>> weekday("28.02.2016")
+    6
     """
-    words = input().lower().split()
-    return words
+    date = date.split(".")
+    day = int(date[0])
+    month = int(date[1])
+    year = int(date[2])
+    if month == 1 or month == 2:
+        month = 12 + month
+        year = year - 1
+    k = year % 100;
+    j = year // 100;
+    h = day + 13 * (month + 1) // 5 + k + k // 4 + j // 4 + 5 * j
+    h = h % 7
+    if h == 1:
+        h = 6
+    elif h == 0:
+        h = 5
+    else: h = h - 2
+    return h
 
+def calendar(month: int, year: int) -> str:
+    """Return a string representing a\
+    horizontal calendar for the given month and year.
 
-def get_pure_user_words(user_words: List[str], letters: List[str],\
-    correct_words: List[str]) -> List[str]:
+    month : an integer in range [1 , 12]
+    year : an integer (strictly speaking the algorithm in weekday
+           works correctly only for Gregorian calendar, so year must
+           be greater than 1583)
+    when arguments are invalid raises AssertionError with corresponding
+    message
+
+    >>> print(calendar(8 , 2015))
+    mon tue wed thu fri sat sun
+                          1   2
+      3   4   5   6   7   8   9
+     10  11  12  13  14  15  16
+     17  18  19  20  21  22  23
+     24  25  26  27  28  29  30
+     31
+    >>> print(calendar(2,2021))
+    mon tue wed thu fri sat sun
+      1   2   3   4   5   6   7
+      8   9  10  11  12  13  14
+     15  16  17  18  19  20  21
+     22  23  24  25  26  27  28
     """
-    (list, list, list) -> list
+    if isinstance(year,int) == False \
+        or isinstance(month,int) == False \
+            or year < 1583 or month < 1 \
+                or month > 12:
+                return AssertionError
+    calend = cal.month(year,month)
+    outcal = "mon tue wed thu fri sat sun\n"
+    count = 0
+    calendik = calend.split("\n")
+    for i in calendik:
+        count += 1
+        if count > 2:
+            if i != calendik[2]:
+                i = i.split()
+                for j in i:
+                    if j == i[0]:
+                        outcal += " "*(3-len(j)) + j
+                    elif j == i[-1]:
+                        outcal += " "*(4-len(j)) + j +"\n"
+                    elif j != i[-1]:
+                        outcal += " "*(4-len(j)) + j
+            elif i == calendik[2]:
+                if i.count("7") != 1:
+                    counter = 0
+                    for digit in i:
+                        if digit.isdigit() == True:
+                            counter += 1
+                    outcal += " "*(4 * (7-counter))
+                    i = i.split()
+                    for j in i:
+                        if j == i[0]:
+                            outcal += " "*(3-len(j)) + j
+                        elif j == i[-1]:
+                            outcal += " "*(4-len(j)) + j +"\n"
+                        elif j != i[-1]:
+                            outcal += " "*(4-len(j)) + j
 
-    Checks user words with the rules and returns list of those words
-    that are not in dictionary.
+                else:
+                    i = i.split()
+                    for j in i:
+                        if j == i[0]:
+                            outcal += " "*(3-len(j)) + j
+                        elif j == i[-1]:
+                            outcal += " "*(4-len(j)) + j +"\n"
+                        elif j != i[-1]:
+                            outcal += " "*(4-len(j)) + j
+    if outcal[-1] == "\n":
+        outcal = outcal[:-1]
+    return outcal
+def transform_calendar(calendar: str) -> str:
     """
-    user_words_fake = []
-    for i in user_words:
-        if i in correct_words:
-            continue
-        user_words_fake.append(i)
+    Return a modified horizontal -> vertical calendar.
 
-    alph = []
-    for i in range(26):
-        alph.append(chr(97 + i))
-    for i in range(9):
-        if letters[i] in alph:
-            try:
-                alph.remove(letters[i])
-            except ValueError:
-                continue
-    words = []
-    for line in user_words_fake:
-        breaking = False
-        line = line.lower()
-        if len(line) < 4:
-            continue
-        if letters[4] in line:
-            for i in range(len(alph)):
-                if alph[i] in line:
-                    breaking = True
+calendar is a string of a calendar, returned by the calendar()
+    function.
+    >>> print(transform_calendar(calendar(5, 2002)))
+    mon   6 13 20 27
+    tue   7 14 21 28
+    wed 1 8 15 22 29
+    thu 2 9 16 23 30
+    fri 3 10 17 24 31
+    sat 4 11 18 25
+    sun 5 12 19 26
+    >>> print(transform_calendar(calendar(8 , 2015)))
+    mon   3 10 17 24 31
+    tue   4 11 18 25
+    wed   5 12 19 26
+    thu   6 13 20 27
+    fri   7 14 21 28
+    sat 1 8 15 22 29
+    sun 2 9 16 23 30
+    """
+    calendar = calendar.split("\n")
+    count = 0
+    calist = []
+    for line in calendar:
+        count += 1
+        line = line.split()
+        if len(line) < 7 and count == 2:
+            for length in range(0,6):
+                line.insert(length,"f")
+                if len(line) == 7:
                     break
-                for j in range(9):
-                    if letters.count(letters[j]) < line.count(letters[j]):
-                        breaking = True
-                        break
-        else:
-            breaking = True
-        if breaking:
-            continue
-        words.append(line)
-    return words
-# get_pure_user_words(["injk",'lskld',"nigga"],["A","G","N","I"])
+        calist.append(line)
+    horizontal = ""
+    for i in calist:
+        if len(i) == 0:
+            calist.remove(i)
+    for element in range(0,len(calist) - 1):
+        if len(calist[element]) < 7:
+            for i in range(0,7):
+                calist[element].append("m")
+                if len(calist[-1]) == 7:
+                    break
+    for elem in range(0,7):
+        for lst in range(0, len(calist) + 1):
+            try:
+                if lst == len(calist) and elem != 6:
+                        horizontal += '\n'
+                if calist[lst][elem] != "m":
+                    if calist[lst][elem] != "f" and lst == len(calist) - 1:
+                        horizontal += calist[lst][elem]
+                    elif calist[lst][elem] != "f" and calist[lst][elem].isdigit() == True:
+                        horizontal += calist[lst][elem] + " "
+                    elif calist[lst][elem] != "f" and calist[lst][elem].isdigit() == False:
+                        horizontal += calist[lst][elem] + " "
+                    else:
+                        horizontal += "  "
+                    pass
+            except:pass
+    if horizontal[-1] == "\n":
+        horizontal = horizontal[:-1]
+    horizontal = horizontal.split('\n')
+    output = ""
+    for line in horizontal:
+        if line.endswith(" "):
+            line = line[:-1]
+        output += line + "\n"
+    if output[-1] == "\n":
+        output = output[:-1]
 
-def results():
-    '''
-    Starts the game
-    '''
-    letters = generate_grid()
-    text = ""
-    for i in range(3):
-        for j in range(3):
-            text += letters[i][j].lower()
-    user_words = get_user_words()
-    correct_words = get_words("en.txt", text)
-    fake_words = get_pure_user_words(user_words, text, correct_words)
-
-    not_entered = correct_words
-    for i in user_words:
-        if i in correct_words:
-            not_entered.remove(i)
-    correct_words_user = len(correct_words) - len(not_entered)
-
-    print(correct_words)
-    print(user_words)
-    print(not_entered)
-
-    with open("result.txt", "w") as result:
-        result.write(str(correct_words_user))
-        result.write("\n")
-        result.write(str(not_entered))
-        result.write("\n")
-        result.write(str(user_words))
-        result.write("\n")
-        result.write(str(fake_words))
-        result.write("\n")
-        result.close()
-results()
+    return output
+# if name == '__main__':
+#     try:
+#         print("Type month")
+#         month = input()
+#         month = int(month)
+#         print("Type year")
+#         year = input()
+#         year = int(year)
+#         print("\n\nThe calendar is: ")
+#         print (calendar(month, year))
+#     except ValueError as err:
+#         print(err)
+print(repr(transform_calendar(calendar(10, 2020))))
--- ЛАБ6/nsk.py
+++ ЛАБ6/nsk.py
@@ -1,8 +1,38 @@
-import urllib.request
+import datetime
+import calendar as calendaR
 
-webpage = 'https://raw.githubusercontent.com/anrom7/Test_Olya/master/New%20folder/total.txt'
-with urllib.request.urlopen(webpage) as web_page:
-    data = []
-    for line in web_page.readlines():
-        data.append(line.decode("utf-8"))
-print(data)
+def calendar(year,month):
+    vertiCal = []
+    cal = calendaR.Calendar(0)
+    for i in cal.itermonthdays2( year, month ):
+        if i[0] == 0:
+            continue
+        else:
+            if   i[1] == 0:
+                theday = "mon"
+            elif i[1] == 1:
+                theday = "mue"
+            elif i[1] == 2:
+                theday = "wed"
+            elif i[1] == 3:
+                theday = "thu"
+            elif i[1] == 4:
+                theday = "fri"
+            elif i[1] == 5:
+                theday = "sat"
+            elif i[1] == 6:
+                theday = "sun"
+            else:
+                print ("Error: Day of week is not between 0 and 6.")
+            theday += " "
+            theday += str(i[0])
+            vertiCal.append(theday)
+    for day in vertiCal:
+        day = day.split()
+        
+
+    out = "The calendar is:"
+    out = out.center(15)
+    print (out)
+    for item in vertiCal: print (item)
+calendar(2021,5)
